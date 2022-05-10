73c1ef44f10f56fea539e35552e520b8bdde8e71
Felipe Dias <felipeferreiradias1@gmail.com>
2022-05-09 21:09:37 -0300

    :rotating_light: Fix: Resolve flake8 problems

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,17 +1,21 @@
 import pandas as pd
 
-# Starting Project
+
 def analyze_log(path_to_file):
     try:
         data = pd.read_csv(path_to_file, header=None)
         data.columns = ["nome", "pedido", "dia"]
-        maria = data[data.nome=="maria"].value_counts(["pedido"]).idxmax()[0]
-        arnaldo = data[(data.nome=="arnaldo") & (data.pedido=="hamburguer")].value_counts(["pedido"]).max()
-        joao_pedidos = set(data["pedido"].unique()) - (set(data.pedido.loc[data.nome=="joao"].unique()))
-        joao_dias = set(data["dia"].unique()) - (set(data.dia.loc[data.nome=="joao"].unique()))
+        maria = data[data.nome == "maria"].value_counts(["pedido"]).idxmax()[0]
+        arnaldo = data[
+            (data.nome == "arnaldo") & (data.pedido == "hamburguer")
+        ].value_counts(["pedido"]).max()
+        joao_pedidos = set(data["pedido"].unique())- (
+            set(data.pedido.loc[data.nome == "joao"].unique()))
+        joao_dias = set(data["dia"].unique()) - (
+            set(data.dia.loc[data.nome == "joao"].unique()))
         with open('data/mkt_campaign.txt', 'w') as file:
             file.write(f'{maria}\n{arnaldo}\n{joao_pedidos}\n{joao_dias}')
-    except:
+    except FileNotFoundError:
         if not path_to_file.endswith(".csv"):
-            raise  FileNotFoundError(f"Extensão inválida: '{path_to_file}'")
+            raise FileNotFoundError(f"Extensão inválida: '{path_to_file}'")
         raise FileNotFoundError(f"Arquivo inexistente: '{path_to_file}'")
--- src/track_orders.py
+++ src/track_orders.py
@@ -1,16 +1,19 @@
 from collections import Counter
 import operator
 
+
 class TrackOrders:
     # aqui deve expor a quantidade de estoque
     def __init__(self):
         self.orders = []
-    
+
     def __len__(self):
         return len(self.orders)
 
     def add_new_order(self, customer, order, day):
-        return self.orders.append({ "customer": customer, "order": order, "day": day })
+        return self.orders.append(
+            {"customer": customer, "order": order, "day": day}
+        )
 
     def get_most_ordered_dish_per_customer(self, customer):
         customer_orders = []
@@ -30,7 +33,6 @@ class TrackOrders:
         new_dict = set(customer_orders)
         return menu - new_dict
 
-
     def get_days_never_visited_per_customer(self, customer):
         menu = set(item["day"] for item in self.orders)
         customer_day = []
@@ -44,12 +46,6 @@ class TrackOrders:
         busy = Counter(item["day"] for item in self.orders)
         busiest_day = max(busy.items(), key=operator.itemgetter(1))[0]
         return busiest_day
-        # customer_day = []
-        # for order in self.orders:
-        #     if order["customer"] == customer:
-        #         customer_day.append(order["day"])
-        # new_dict = set(customer_day)
-        # return menu - new_dict
 
     def get_least_busy_day(self):
         busy = Counter(item["day"] for item in self.orders)
